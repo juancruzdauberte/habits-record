3b04d7432a7ed01700aa717e920bf602a5ee478d
Juan Cruz Dauberte <juandauberte@gmail.com>
2025-04-22 20:26:43 -0300

    feat: variables de entorno

--- src/components/config/db.ts
+++ src/components/config/db.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  import.meta.env.VITE_APP_URL,
-  import.meta.env.VITE_APP_API_KEY
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
